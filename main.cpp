3bc97dc6d0057e30f5423e27872364a974a0875b
jonluu1 <jonluu1@gmail.com>
2022-02-06 20:45:49 -0800

    branch and pull test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
-#include <stdio.h> 
+#include <iostream>
 
 int main(void) {
-  int userInput = 0;
-  
-  scanf("This is the test commit, make a branch, make changes and then add to this file\n how big do you want the box?", userInput);
-  char out[userInput];
-  for (int i = 0; i < userInput; i++) { out[i] = '#'; }
-  
-  for (int i = 0; i < userInput; i++) { printf("%s\n", out); }
-  return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
