76a9944a1aa51acab7ad1a5f0ffba7c64393ca15
ssm498 <s.fernando@ufl.com>
2022-03-09 14:33:05 -0600

    Cleaned up commented out practice code

--- src/App.js
+++ src/App.js
@@ -7,29 +7,3 @@ function App() {
 }
 
 export default App
-/*const containerStyle = {
-  width: '800px',
-  height: '500px',
-  position: 'absolute',
-  left: '50%',
-  top: '25%',
-  transform: 'translate(-50%, -50%)',
-}
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-}
-
-function Map() {
-  return (
-    <LoadScript googleMapsApiKey=''>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        {/* Child components, such as markers, info windows, etc. }
-        <></>
-      </GoogleMap>
-    </LoadScript>
-  )
-}
-
-export default React.memo(Map)*/
