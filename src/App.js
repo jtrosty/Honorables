121269752d4e2b0fd11f9f190859f728f7308337
ssm498 <s.fernando@ufl.com>
2022-03-13 15:03:05 -0500

    Added 'google places' and 'auto-complete' apis, Also added a search bar for the google map, changed name of Map.js to GoogleMap.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // Component imports
 import Home from './components/Home'
-import Map from './components/Map'
+import GoogleMap from './components/GoogleMap'
 import Navbar from './components/Navbar'
 import Messages from './components/Messages'
 
 function App() {
   return (
-    <div className="container">
-      <Map />
-      <Router> 
-        <Navbar/>
-        <Switch>
+    <div className='container'>
+      <GoogleMap />
+      <Router>
+        <Navbar />
+        <Routes>
           <Route path='/' exact elemetn={Home} />
           <Route path='/Messages' exact element={Messages} />
-        </Switch>
+        </Routes>
       </Router>
     </div>
   )
