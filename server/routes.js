fcdcb1cb1f13309efe85c384b3249aa0dd44aec7
ssm498 <s.fernando@ufl.com>
2022-03-28 16:35:53 -0500

    Added routes.js into the 'server' file. These are the endpoints to get user and put user

--- server/routes.js
+++ server/routes.js
@@ -1,1 +1,29 @@
+// Load express and the schema created
+const express = require('express')
+const Student = require('./models')
+const app = express()
 
+// Creates POST endpoint
+app.post('/add_user', async (request, response) => {
+  const user = new Student(request.body)
+
+  try {
+    await user.save()
+    response.send(user)
+  } catch (error) {
+    response.status(500).send(error)
+  }
+})
+
+// Creates GET endpoint
+app.get('/users', async (request, response) => {
+  const users = await userModel.find({})
+
+  try {
+    response.send(users)
+  } catch (error) {
+    response.status(500).send(error)
+  }
+})
+
+module.exports = app
